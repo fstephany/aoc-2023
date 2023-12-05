5af8e9ebde6a46de9a7f19a784595b6a74eaeaea
Francois Stephany <francois@tamere.eu>
2023-12-05 22:16:20 +0100

    Starting to work on Part 2 of day 5

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,6 +1,9 @@
+use std::{collections::BTreeSet, ops::Range};
+
 fn main() {
     let file_content = std::fs::read_to_string("inputs/day5").unwrap();
     println!("Part 1: {}", part_one(&file_content));
+    // println!("Part 2: {}", part_two(&file_content));
 }
 
 #[derive(Default, Debug)]
@@ -77,7 +80,7 @@ fn part_one(input: &str) -> u64 {
 
     for line in lines.map(|l| l.trim()) {
         if line.is_empty() {
-            dbg!(&current_category);
+            // dbg!(&current_category);
             if !current_category.mappings.is_empty() {
                 almanach.categories.push(current_category)
             }
@@ -115,8 +118,83 @@ fn part_one(input: &str) -> u64 {
         .collect();
 
     dbg!(&end_results);
-
     end_results.into_iter().min().unwrap()
+}
+
+fn part_two(input: &str) -> u64 {
+    let mut lines = input.lines();
+
+    let seeds_data: Vec<u64> = lines
+        .next()
+        .unwrap()
+        .trim()
+        .strip_prefix("seeds: ")
+        .unwrap()
+        .split_ascii_whitespace()
+        .map(|n| n.parse::<u64>().unwrap())
+        .collect();
+
+    let seed_ranges: Vec<Range<u64>> = seeds_data
+        .chunks(2)
+        .map(|chunk| (chunk[0]..chunk[0] + chunk[1]))
+        .collect();
+
+    dbg!(&seed_ranges);
+
+    let mut almanach = Almanach::default();
+    let mut current_category = CategoryMapping::default();
+
+    for line in lines.map(|l| l.trim()) {
+        if line.is_empty() {
+            // dbg!(&current_category);
+            if !current_category.mappings.is_empty() {
+                almanach.categories.push(current_category)
+            }
+
+            // We start a new map definition
+            current_category = CategoryMapping::default();
+        } else if line.starts_with(|c: char| c.is_ascii_alphabetic()) {
+            current_category.name = line.to_owned();
+        } else if line.starts_with(|c: char| c.is_ascii_digit()) {
+            let mut parts = line
+                .split_ascii_whitespace()
+                .map(|n| n.parse::<u64>().unwrap());
+
+            current_category.mappings.push(Mapping::new(
+                parts.next().unwrap(), // Destination
+                parts.next().unwrap(), // Source
+                parts.next().unwrap(), // Range
+            ))
+        } else {
+            unreachable!("Invalid input: '{line}'");
+        }
+    }
+
+    // Handle last line. Ugly but hey.
+    almanach.categories.push(current_category);
+
+    // Do the seed range overlap? Put them in a set to be sure.
+
+    let all_ranges: BTreeSet<u64> = seed_ranges
+        .into_iter()
+        .flat_map(|range| range.collect::<Vec<u64>>())
+        .collect();
+
+    dbg!(all_ranges.len());
+
+    // let end_results: Vec<u64> = seeds
+    //     .into_iter()
+    //     .map(|seed| {
+    //         // do the full path
+    //         almanach.traverse(seed)
+    //     })
+    //     .collect();
+
+    // dbg!(&end_results);
+
+    // end_results.into_iter().min().unwrap()
+
+    42
 }
 
 #[cfg(test)]
@@ -160,5 +238,50 @@ mod tests {
         60 56 37
         56 93 4";
         assert_eq!(part_one(input), 35);
+    }
+
+    #[test]
+    fn real_part1() {
+        let input = std::fs::read_to_string("inputs/day5").unwrap();
+        assert_eq!(part_one(&input), 226172555);
+    }
+
+    #[test]
+    fn example_part_2() {
+        let input = "\
+        seeds: 79 14 55 13
+
+        seed-to-soil map:
+        50 98 2
+        52 50 48
+        
+        soil-to-fertilizer map:
+        0 15 37
+        37 52 2
+        39 0 15
+        
+        fertilizer-to-water map:
+        49 53 8
+        0 11 42
+        42 0 7
+        57 7 4
+        
+        water-to-light map:
+        88 18 7
+        18 25 70
+        
+        light-to-temperature map:
+        45 77 23
+        81 45 19
+        68 64 13
+        
+        temperature-to-humidity map:
+        0 69 1
+        1 0 69
+        
+        humidity-to-location map:
+        60 56 37
+        56 93 4";
+        assert_eq!(part_two(input), 46);
     }
 }
